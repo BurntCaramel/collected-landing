528ea1b52a5ed2f536f0da7ed236546d3679f22d
Patrick Smith <patrick@burntcaramel.com>
2018-04-07 01:53:29 +1000

    Add icons for GitHub, contribute

--- src/pages/research.tsx
+++ src/pages/research.tsx
@@ -135,6 +135,7 @@ const isPrimaryItem = ({ tags }: { tags: [string] }): boolean =>
 const regexToFontAwesomeName = [
   // Brands
   { regex: /\bfacebook\b/i, className: 'fab fa-facebook' },
+  { regex: /\bgithub\b/i, className: 'fab fa-github' },
   { regex: /\binstagram\b/i, className: 'fab fa-instagram' },
   { regex: /\bmedium\b/i, className: 'fab fa-medium' },
   { regex: /\btwitter\b/i, className: 'fab fa-twitter' },
@@ -143,6 +144,7 @@ const regexToFontAwesomeName = [
   { regex: /\bnew|add\b/i, className: 'fas fa-plus' },
   { regex: /\buser|profile|account\b/i, className: 'fas fa-user-circle' },
   { regex: /\bsearch\b/i, className: 'fas fa-search' },
+  { regex: /\bcontribute\b/i, className: 'fas fa-pencil-alt' },
 ]
 
 function Icon({ text, sizeRem = 1, fallbackRounded = false }: { text: string, sizeRem?: number, fallbackRounded?: boolean }) {
