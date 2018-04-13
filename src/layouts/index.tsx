63c4c5d8818a63855cf244d4bcd1fa3bb3abd14d
Patrick Smith <patrick@burntcaramel.com>
2018-04-14 01:34:18 +1000

    Fix minor attribute name issue

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -36,7 +36,7 @@ class TemplateWrapper extends React.Component<Props, {}> {
               rel: "stylesheet",
               href: "https://use.fontawesome.com/releases/v5.0.9/css/all.css",
               integrity: "sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1",
-              crossorigin: "anonymous"
+              crossOrigin: "anonymous"
             }
           ]}
         />
